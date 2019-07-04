d2f111510c340abd2fdd3662f03f1ecefe57c5f6
Dotsung <andy395023@gmail.com>
2019-07-04 20:44:32 +0900

    api 분리

---
+++ src/schema.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import { makeExecutableSchema } from 'graphql-tools';
+import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
+
+// **: 모든 폴더  *.graphql: 모든 .graphql파일
+const allTypes = fileLoader(path.join(__dirname, '/api/**/*.graphql'));
+const allResolvers = fileLoader(path.join(__dirname, '/api/**/*.js'));
+
+const schema = makeExecutableSchema({
+    typeDefs: mergeTypes(allTypes),
+    resolvers: mergeResolvers(allResolvers)
+});
+
+export default schema;
